8437680eb43982c6898d2707a7defa15db2c146f
anushab7 <69648384+anushab7@users.noreply.github.com>
2022-02-11 14:06:54 -0500

    Update Startup.cs

--- PostgreSqlDotnetCore/Startup.cs
+++ PostgreSqlDotnetCore/Startup.cs
@@ -30,7 +30,7 @@ namespace PostgreSqlDotnetCore
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
